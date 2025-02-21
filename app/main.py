84b0abe21189525107d969857a0b890e8fbebea9
Ato Toffah <atotoffah@gmail.com>
2025-02-21 21:04:37 +0000

    feat: main.py run

--- app/main.py
+++ app/main.py
@@ -37,3 +37,9 @@ async def id_exception_handler(request, exc):
         status_code=status.HTTP_400_BAD_REQUEST,
         content={"detail": "Invalid ID"},
     )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
